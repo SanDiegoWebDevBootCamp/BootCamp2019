97b4ad14825e5ae3c059f64c4ba32f3b1a7b44b5
Sang Yum <Sang.Yum@walmartlabs.com>
2019-11-10 16:57:16 -0800

    Rearranged the content

---
+++ week01/fizzBuzz.js
@@ -0,0 +1,26 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const isMultipleOfThree = (number) => number % 3 === 0
+const isMultipleOfFive = (number) => number % 5 === 0
+
+function printFizzBuzz(number) {
+    if (isMultipleOfFive(number) && isMultipleOfThree(number)) {
+        console.log('FizzBuzz')
+    } else if (isMultipleOfThree(number)) {
+        console.log('Fizz')
+    } else if (isMultipleOfFive(number)) {
+        console.log('Buzz')
+    } else {
+        console.log(number)
+    }
+}
+
+const numbers = []
+var i
+for (i = 0; i < 100; i++) {
+   numbers[i] = getRandomInt(1000)
+}
+
+numbers.forEach(number => printFizzBuzz(number))
